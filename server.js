82cdba313f2fb172facbee0d8ad231831fdd5189
saba-karbakhsh <143356265+saba-karbakhsh@users.noreply.github.com>
2025-02-20 08:51:44 -0800

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ let url = require('url');
 let messages = require('./messages');
 
 
-let connectionString = "mysql://doadmin:AVNS_9SRi_k8cAXyPWn39Gp7@db-mysql-tor1-19416-do-user-18794098-0.d.db.ondigitalocean.com:25060/defaultdb?ssl-mode=REQUIRED";
+let connectionString = "mysql://user:AVNS_7vzZaIcup4ykiX08DQy@db-mysql-tor1-19416-do-user-18794098-0.d.db.ondigitalocean.com:25060/defaultdb?ssl-mode=REQUIRED";
 let con = db.createConnection(connectionString);
 
 con.connect(function (err) {
